10cc3dc93c3de05bbef534f54706b5bf650ed03a
saienko-ana <saienko_ana@icloud.com>
2021-12-06 14:35:56 +0100

    added js code for ACcordeon

--- js/script.js
+++ js/script.js
@@ -14,60 +14,28 @@ if (iconMenu) {
 }
 
 // ---------------------------------------------------------
-// https://medium.com/@vadimfilimonov/%D0%B0%D0%BA%D0%BA%D0%BE%D1%80%D0%B4%D0%B5%D0%BE%D0%BD-%D0%BD%D0%B0-%D1%87%D0%B8%D1%81%D1%82%D0%BE%D0%BC-js-287b455ac433
 
-function findElements(object, element) {
-	const instance = object;
-	instance.element = element;
-	instance.target = element.nextElementSibling;
-}
+document.addEventListener('DOMContentLoaded', () => {
+	const accordions = document.querySelectorAll('.accordion');
 
-function hideElement(object) {
-	const instance = object;
-	const { target } = instance;
-	target.style.height = null;
-	instance.isActive = false;
-}
+	accordions.forEach(el => {
+		el.addEventListener('click', (e) => {
+			const self = e.currentTarget;
+			const control = self.querySelector('.accordion__control');
+			const content = self.querySelector('.accordion__content');
 
-function showElement(object) {
-	const instance = object;
-	const { target, height } = instance;
-	target.style.height = `${height}px`;
-	instance.isActive = true;
-}
+			self.classList.toggle('open');
 
-function changeElementStatus(instance) {
-	if (instance.isActive) {
-		hideElement(instance);
-	} else {
-		showElement(instance);
-	}
-}
-
-function measureHeight(object) {
-	const instance = object;
-	instance.height = object.target.firstElementChild.clientHeight;
-}
-
-function subscribe(instance) {
-	instance.element.addEventListener('click', (event) => {
-		event.preventDefault();
-		changeElementStatus(instance);
+			// если открыт аккордеон
+			if (self.classList.contains('open')) {
+				control.setAttribute('aria-expanded', true);
+				content.setAttribute('aria-hidden', false);
+				content.style.maxHeight = content.scrollHeight + 'px';
+			} else {
+				control.setAttribute('aria-expanded', false);
+				content.setAttribute('aria-hidden', true);
+				content.style.maxHeight = null;
+			}
+		});
 	});
-	window.addEventListener('resize', () => measureHeight(instance));
-}
-
-function accordion(element) {
-	const instance = {};
-		
-	function init() {
-		findElements(instance, element);
-		measureHeight(instance);
-		subscribe(instance);
-	}
-	
-	init();
-}
-
-const elements = [...document.querySelectorAll('.js-accordion')];
-elements.forEach(accordion);
+});
